5a4c6d12baebe1496d9677200b10cd45dac61b7a
dulajkavinda <dulajkaavinda@gmail.com>
2021-07-20 22:05:35 +0530

    commit with erros

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  var foo = "bar";
   return (
     <div className="App">
       <header className="App-header">
